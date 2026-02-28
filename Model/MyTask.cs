8ed8544f14ddce2e53ffaa5277c11633cf22a71d
vuongnd9 <nguyendinhvuong.act@gmail.com>
2026-02-28 22:51:44 +0700

    initialize project

---
+++ Model/MyTask.cs
@@ -0,0 +1,15 @@
+namespace TaskTracker.Model
+{
+  public class MyTask
+  {
+    public MyTask()
+    {
+      CreatedDate = DateTime.Now;
+    }
+    public int Id { get; set; }
+    public string? Description { get; set; }
+    public string? Status { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? Updated
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using TaskTracker.Model;
+
+namespace TaskTracker;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var ahihi = new MyTask();
+    }
+}
