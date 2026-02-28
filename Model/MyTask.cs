2f29706dd36d20df87a15c6a0f33e2dfcb7ef1fb
vuongnd9 <nguyendinhvuong.act@gmail.com>
2026-02-28 22:53:41 +0700

    add gitignore

--- Model/MyTask.cs
+++ Model/MyTask.cs
@@ -5,11 +5,12 @@ namespace TaskTracker.Model
     public MyTask()
     {
       CreatedDate = DateTime.Now;
+      Status = "todo";
     }
     public int Id { get; set; }
     public string? Description { get; set; }
-    public string? Status { get; set; }
+    public string Status { get; set; }
     public DateTime CreatedDate { get; set; }
-    public DateTime? Updated
+    public DateTime? UpdatedAt { get; set; }
   }
 }
--- obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed8544f14ddce2e53ffaa5277c11633cf22a71d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
